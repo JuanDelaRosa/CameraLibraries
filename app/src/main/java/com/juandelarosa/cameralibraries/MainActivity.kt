4abfa23eea237fa42c0e2cca1d4bf33dbe30328d
Juan Conrado Herrera De la Rosa <juan-delarosa@outlook.com>
2021-06-04 08:48:40 -0500

    Implementation of CameraX to take pictures and switch Front and Back Camera

--- app/src/main/java/com/juandelarosa/cameralibraries/MainActivity.kt
+++ app/src/main/java/com/juandelarosa/cameralibraries/MainActivity.kt
@@ -1,11 +1,159 @@
 package com.juandelarosa.cameralibraries
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import android.util.Size
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.*
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.juandelarosa.cameralibraries.databinding.ActivityMainBinding
+import com.juandelarosa.cameralibraries.util.Constants
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
 
 class MainActivity : AppCompatActivity() {
+
+
+    private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    private var cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+    private var imageCapture : ImageCapture? = null
+    private lateinit var outputDirectory : File
+
+    private fun getOutputDirectory(): File {
+        val mediaDir = externalMediaDirs.firstOrNull()?.let{ mFile -> File(mFile, resources.getString(R.string.app_name)).apply {
+            mkdir()
+        }}
+        return if(mediaDir != null && mediaDir.exists()) mediaDir else filesDir
+    }
+
+    private lateinit var cameraExecutor : ExecutorService
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        if (allPermisionGranted()) {
+            startCamera()
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                Constants.REQUIRED_PERMISSIONS,
+                Constants.REQUEST_CODE_PERMISSIONS
+            )
+        }
+        cameraExecutor = Executors.newSingleThreadExecutor()
+
+        outputDirectory = getOutputDirectory()
+        binding.takePhotoBtn.setOnClickListener{
+            takePhoto()
+        }
+
+        binding.changeCameraBtn.setOnClickListener {
+            switchCamera()
+        }
+    }
+
+    private fun switchCamera() {
+        cameraSelector = if(cameraSelector == CameraSelector.DEFAULT_FRONT_CAMERA)
+            CameraSelector.DEFAULT_BACK_CAMERA
+        else
+            CameraSelector.DEFAULT_FRONT_CAMERA
+        startCamera()
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener({
+            // Used to bind the lifecycle of cameras to the lifecycle owner
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            // Preview
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(binding.cameraView.surfaceProvider)
+                }
+
+            imageCapture = ImageCapture.Builder()
+                .build()
+
+            val imageAnalyzer = ImageAnalysis.Builder()
+                .setTargetResolution(Size(1080, 1920))
+                .build()
+
+            try {
+                cameraProvider.unbindAll()
+
+                cameraProvider.bindToLifecycle(
+                    this, cameraSelector, preview, imageCapture, imageAnalyzer
+                )
+
+
+            } catch (exc: Exception) {
+                Log.e(Constants.TAG, "Use case binding failed", exc)
+            }
+
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+    private fun takePhoto(){
+        val imageCapture = imageCapture ?: return
+        val photoFile =File(
+            outputDirectory,
+            SimpleDateFormat(Constants.FILE_NAME_FORMAT, Locale.US).format(System.currentTimeMillis()) + ".jpg")
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+
+        imageCapture.takePicture(outputOptions,ContextCompat.getMainExecutor(this),
+            object : ImageCapture.OnImageSavedCallback{
+                override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                    val saveUri = Uri.fromFile(photoFile)
+                    val msg = "Photo Saved"
+                    Toast.makeText(this@MainActivity,"$msg $saveUri", Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onError(exception: ImageCaptureException) {
+                    Log.e(Constants.TAG, "onError: ${exception.message}", exception)
+                }
+        })
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
+    }
+
+    private fun allPermisionGranted(): Boolean =
+        Constants.REQUIRED_PERMISSIONS.all{
+            ContextCompat.checkSelfPermission(
+                baseContext,it
+            ) == PackageManager.PERMISSION_GRANTED
+        }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == Constants.REQUEST_CODE_PERMISSIONS){
+            if(allPermisionGranted()){
+                startCamera()
+            }else{
+                Toast.makeText(this, "Permission not granted by the user", Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
     }
 }
